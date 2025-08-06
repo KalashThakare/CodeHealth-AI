f7579ce06e72a803e8945a70a5dd31ee52e87152
KalashT <kalashthakare898@gmail.com>
2025-08-06 07:54:14 +0530

    manage-permissions API created

--- backend/src/controller/gitHub.Controller.js
+++ backend/src/controller/gitHub.Controller.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { generateState, generateCodeVerifier } from "arctic";
+import { github } from "../lib/OAuth/github.js";
 
 export const ListRepos = async (req, res) => {
     try {
@@ -22,5 +24,36 @@ export const ListRepos = async (req, res) => {
         console.error('GitHub API error:', error.response?.data || error.message);
     res.status(500).json({ error: 'Failed to fetch repositories' });
 
+    }
+}
+
+export const manageGitHubScopes =async(req,res)=>{
+    try {
+
+        const state = generateState();
+        const codeVerifier = generateCodeVerifier();
+        const scopes = ["repo", "read:user", "user:email"];
+
+        res.cookie("github_oauth_state", state, {
+            httpOnly: true,
+            maxAge: 600000,
+            sameSite: "lax",
+        });
+        res.cookie("github_code_verifier", codeVerifier, {
+            httpOnly: true,
+            maxAge: 600000,
+            sameSite: "lax",
+        });
+
+  
+        const authURL = github.createAuthorizationURL(state, scopes);
+
+        res.redirect(authURL);
+        
+    } catch (error) {
+
+        console.error('GitHub API error:', error.response?.data || error.message);
+        res.status(500).json({ error: 'Internal server error' });
+        
     }
 }
--- backend/src/routes/githubRoutes.js
+++ backend/src/routes/githubRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { ListRepos } from "../controller/gitHub.Controller.js";
+import { ListRepos, manageGitHubScopes } from "../controller/gitHub.Controller.js";
 
 const router = express.Router();
 
 router.get("/users/:username/repos",ListRepos)
+router.get('/permissions', manageGitHubScopes);
 
 export default router;
