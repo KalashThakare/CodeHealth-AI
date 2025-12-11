544ffd9c7be885b290e5be7a20eab2a027ab2893
KalashT <kalashthakare898@gmail.com>
2025-12-12 02:38:49 +0530

    created recent preview controller

--- backend/src/controller/dashbord.Controller.js
+++ backend/src/controller/dashbord.Controller.js
@@ -34,3 +34,45 @@ export const getUsage = async(req,res)=>{
         return res.status(500).json({message:"Internal server error"},error);
     }
 }
+
+export const getRecentPreview = async (req, res) => {
+  try {
+    const userId = req.user?.id;
+
+    if (!userId) {
+      return res.status(400).json({ error: "Unauthorised" });
+    }
+
+    const recentRepo = await Project.findOne({
+      where: {
+        userId: userId,
+        initialised: true,
+        analysisStatus: 'completed' 
+      },
+      attributes: ['repoId', 'fullName', 'repoName', 'analysisCompletedAt'],
+      order: [['analysisCompletedAt', 'DESC']] 
+    });
+
+    if (!recentRepo) {
+      return res.status(200).json({
+        message: "No initialized repositories found",
+        metrics: null
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      userId: userId,
+      fullName:recentRepo.fullName,
+      repoName:recentRepo.repoName,
+      analysisCompletedAt:recentRepo.analysisCompletedAt
+    });
+
+  } catch (error) {
+    console.error("Error fetching recent metrics preview:", error);
+    return res.status(500).json({
+      error: "Failed to fetch recent metrics",
+      details: error.message
+    });
+  }
+};
--- backend/src/routes/dashboard.Routes.js
+++ backend/src/routes/dashboard.Routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getUsage } from "../controller/dashbord.Controller.js";
+import { getRecentPreview, getUsage } from "../controller/dashbord.Controller.js";
 
 const router = express.Router();
 
 router.get("/usage",protectRoute, getUsage);
+router.get("/recentPreview", protectRoute, getRecentPreview);
 
 export default router;
