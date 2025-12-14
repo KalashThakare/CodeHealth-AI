24201654bd16bbd4f7c0ea465c0e677cd1e4946a
KalashT <kalashthakare898@gmail.com>
2025-12-14 22:45:29 +0530

    cors updated

--- python-server/app/core/config.py
+++ python-server/app/core/config.py
@@ -3,12 +3,17 @@ from functools import lru_cache
 from pydantic import Field, ConfigDict
 
 class Settings(BaseSettings):
-    PORT: int = Field(env="PORT")
-    ALLOWED_ORIGINS: list[str] = ["http://localhost:3000", "http://localhost:8080"]
+    PORT: int = Field(...,env="PORT")
+    EXPRESS_URL: str = Field(..., env="EXPRESS_URL")
     model_config = ConfigDict(
         extra="ignore",  
         env_file=".env",   
     )
+
+    @property
+    def ALLOWED_ORIGINS(self) -> list[str]:
+        return [self.EXPRESS_URL]
+
 
 @lru_cache
 def get_settings() -> Settings:
